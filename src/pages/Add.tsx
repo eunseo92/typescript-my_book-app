be6c42822ab6b3d963364e538fb488459baf5015
Eunseo Park <eunseopark@Eunseos-MacBook-Pro.local>
2021-06-11 09:56:38 +0900

    initial project(API Error)

---
+++ src/pages/Add.tsx
@@ -0,0 +1,12 @@
+import { Redirect } from 'react-router'
+import AddContainer from '../containers/AddContainer'
+import useToken from '../hooks/useToken'
+
+export default function Add() {
+  const token = useToken()
+
+  if(token === null) {
+    return <Redirect to='/signin' />
+  }
+  return <AddContainer />
+}
---
+++ src/pages/Signin.tsx
@@ -0,0 +1,11 @@
+import { Redirect } from 'react-router'
+import SigninContainer from '../containers/SigninContainer'
+import useToken from '../hooks/useToken'
+
+export default function Signin() {
+  const token = useToken()
+  if (token !== null) {
+    return <Redirect to="/" />
+  }
+  return <SigninContainer />
+}
